a137b6ca04ba62a915f161e89d2871931725c488
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-09-11 11:31:57 +0200

    media queries

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ app.use(express.static(publicPath));
 
 app.get("/man",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/man.html"));
-})
+});
 app.get("/woman",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/woman.html"));
-})
+});
 app.get("/history",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/history.html"));
-})
+});
 app.get("/expertise",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/expertise.html"));
-})
+});
 app.get("/shops",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/shop.html"));
-})
+});
 app.get("/concordia",(req,res,next)=>{
     res.sendFile(path.join(__dirname , "/concordia.html"));
-})
+});
 
 
 app.listen(port, () =>{
