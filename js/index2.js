196763dd74053b63c31d21a98b814e697d1b8998
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-09-12 00:31:15 +0200

    now only the video

--- js/index2.js
+++ js/index2.js
@@ -4,7 +4,7 @@
     var url = new URL(url_string);
     var modelIndex = url.searchParams.get("modelIndex");
     var modelColors = url.searchParams.get("modelColors").split(",");
-    
+
     $(".watch-img").attr("src",`img/watch/watch${modelIndex}/watch${modelIndex}.png`)
     for (let item of modelColors) {
         $(".watch__sale-color-box").append(`<img src="img/colors/${item}.png" alt="image for watch ${item}" class="watch__collar-colors">`)
